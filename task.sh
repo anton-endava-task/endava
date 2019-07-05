997f63692c02d7080f5ee6c9c3902c536826baa9
anton-endava-task <boyanov.anton@gmail.com>
2019-07-05 21:22:05 +0300

    documentation added

--- task.sh
+++ task.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-
+                                            ## Making a folder for downloaded files
 FOLDER=task_downloads
 COUNTER=0
-
 mkdir $FOLDER
-
+                                            ## Making a loop that read a file with 10 websites called weblist
 while read p; do
-
+                                            ## Write the content of each website index file to a file called "index_" + numer
   wget $p -O "$FOLDER"/"index"_"$COUNTER"
   COUNTER=$((COUNTER+1))
-
-done <weblist
-
-for f in "$FOLDER"/*
+done <weblist                               ## The result is a list of files called "index_0", "index_1" ... All in my "task_downloads" folder
+                                            ## Now I`ll read all files one by one and count the lines including "href=" text 
+for f in "$FOLDER"/* ## Making for loop for all files in my folder
 do
-  grep -r "href=" $f | wc -l > "$f"_"count"
-done
-
-
-##$(ls -t | head -n1)
+  grep -r "href=" $f | wc -l > "$f"_"count" ## count the lines with "href=" and write the result to a file named source file + "_count"
+done                                        ## The result is new 10 files created in the same folder called "index_0_count", "index_1_count" ...
+                                            ## Now its time to delete the newly created 10 files with results
+                                            ## I enter the folder with all created files, sort the list by time creation and remove the newly 10 files
 cd $FOLDER
-ls -alt
-rm -f $(ls -t | head -n10)
+rm -f $(ls -t | head -n10)                  ## Done
+                                            ## You can measure execution time by executing the script like this:# timer task.sh 
 
-##rm $(ls -Art /"$FOLDER"/ | tail -n +5)
